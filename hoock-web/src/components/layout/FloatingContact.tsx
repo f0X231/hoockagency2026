ad602f7bd908c56ae182d284b3cfa41875a5c051
zhaii <guChai@gmail.com>
2026-03-28 22:11:14 +0700

    fix icon phone

--- hoock-web/src/components/layout/FloatingContact.tsx
+++ hoock-web/src/components/layout/FloatingContact.tsx
@@ -87,20 +87,17 @@ export default function FloatingContact() {
       {/* Phone */}
       <a
         href="tel:0870036751"
-        className="flex h-[45px] w-[45px] items-center justify-center rounded-full bg-black text-white shadow-md transition-transform hover:scale-110"
+        className="flex h-[45px] w-[45px] items-center justify-center rounded-full bg-black text-white shadow-md transition-transform hover:scale-110 ring-1 ring-white/20"
         aria-label="Call us"
         title="โทรหาเรา"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          className="h-6 w-6"
+          className="h-8 w-8"
           fill="currentColor"
         >
-          <path
-            d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1v3.49c0 .55-.45 1-1 1C19.39 21 11 12.61 11 2.61c0-.55.45-1 1-1h3.49c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"
-            transform="translate(-2, -2) scale(1.15)"
-          />
+          <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1v3.49c0 .55-.45 1-1 1C19.39 21 11 12.61 11 2.61c0-.55.45-1 1-1h3.49c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
         </svg>
       </a>
     </div>
